a63e96bb2e2d9e0a1176779186bd572008ebc0e6
sphnikola <sphnikola@gmail.com>
2024-01-13 00:04:31 +0000

    Add font and space properties

--- src/Navabr.jsx
+++ src/Navabr.jsx
@@ -2,7 +2,7 @@ export function Navbar() {
   return (
     <>
       <div className="flex items-center justify-between px-3">
-        <h1>Devfinder</h1>
+        <h1 className="text-[2em] font-bold">Devfinder</h1>
       </div>
     </>
   );
